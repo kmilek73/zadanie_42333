10d63b4c202c0fcd0c9c695454c61d84498e2af0
kmilek <kmilek73@gmail.com>
2022-12-05 19:53:16 +0100

    rozwiazanie

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+
+let tekst_1 = "Uwielbiam Javascript";
+let tekst_2 = "Będę świetnym programistą :-)";
+
+if (tekst_2.length >tekst_1.length){
+    console.log("Dłuższy jest tekst:", tekst_2)
+}else{
+    console.log("Dłuższy jest tekst:", tekst_1)
+ }
+
+
+
